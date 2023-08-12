570a40213025d389f6a62379b8c3b9821f64aa73
kobayashi <823913645@qq.com>
2023-08-12 15:56:23 +0800

    fix: markdown style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'vitesse-dark',
+      syntaxHighlight: "shiki",
+    }
+  }
 });
