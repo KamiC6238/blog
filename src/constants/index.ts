aed9b97ef489e32cc73ea697c610b483d891cac2
kobayashi <823913645@qq.com>
2023-08-12 22:59:25 +0800

    refactor: projects

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,20 @@
 export const DARK_MODE_KEY = 'KOBAYASHI_MODE';
 export const DARK = 'dark';
 export const LIGHT = 'light';
+
+export const ProjectConfigs = [
+    {
+        name: 'monaco-breakpoints',
+        href: 'https://github.com/KamiC6238/monaco-breakpoints',
+        content: 'A type-safe library support breakpoints in monaco-editor like vscode'
+    },
+    {
+        name: 'Blog',
+        href: 'https://github.com/KamiC6238/blog',
+        content: 'A blog developed using ',
+        aTagInfo: {
+            href: 'https://astro.build/',
+            content: 'Astro'
+        },
+    }
+];
