3983d1fa0489d8ceefea9d392e5e4403d40bc1cb
kobayashi <823913645@qq.com>
2023-08-12 16:12:50 +0800

    build: remove useless package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind()],
   markdown: {
     shikiConfig: {
       theme: 'vitesse-dark',
